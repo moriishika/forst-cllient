d16bf200e0498792291b0131fd8a37b54a2a4005
Morii Shikaa <48641207+moriishika@users.noreply.github.com>
2023-05-30 15:34:10 +1000

    text editor component created

---
+++ components/TextEditor/index.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Typography from "@tiptap/extension-typography";
+
+const ConfiguredStarterKit = StarterKit.configure({
+  heading: {
+    levels: [1, 2, 3, 4, 5, 6]
+  }
+})
+
+const TextEditor = () => {
+
+  const editor = useEditor({
+    content: `<h1>Helllo</h1> <h2>Keren</h2>`,
+    extensions: [ConfiguredStarterKit, Typography],
+    editorProps: {
+      attributes: {
+        class: "bg-red-500 prose dark:prose-invert prose-sm sm:prose-base  prose-headings:m-0 prose-p:m-0 prose-p:leading-none  m-5 focus:outline-none"
+      }
+    }
+  });
+
+  return (
+    <div>
+      <h1>Keren</h1>
+      <h2>KEren</h2>
+      <EditorContent editor={editor}></EditorContent>
+    </div>
+  );
+}
+
+export default TextEditor;
