6776fda1e23724167ad6da5a0dc7083c1b495023
Morii Shikaa <48641207+moriishika@users.noreply.github.com>
2023-06-08 18:12:10 +1000

    Basic styling for stories section is done

--- components/StoriesComponents/StoriesSection/index.tsx
+++ components/StoriesComponents/StoriesSection/index.tsx
@@ -1,6 +1,6 @@
 import PageIndicator from "../../PageIndicator";
 
-const ColumnIndicator = ( ) => {
+const ColumnIndicator = () => {
   return (
     <div>
       <button>Prev</button>
@@ -10,19 +10,30 @@ const ColumnIndicator = ( ) => {
   )
 }
 
+const StoryBox = () => {
+  return (
+    <div className="p-4 bg-purple-200 flex flex-col justify-center align-middle w-auto h-auto rounded-lg">
+      <img src="/example.jpg" width={300} className="m-auto"></img>
+      <h2>Aesther and Edelweis</h2>
+    </div>
+  )
+}
+
 const StoriesSection = () => {
-  return(
-    <>
-      <div className="min-h-screen bg-green-100 w-full">
-        <h1>Stories</h1>
-      </div>
-      <div>
-        <div>Story 1 Box</div>
-        <div>Story 2 Box</div>
-        <ColumnIndicator></ColumnIndicator>
+  return (
+    <div className="flex flex-col space-y-5">
+      <div className="m-auto space-y-5">
+        <div className="prose">
+          <h1>Stories</h1>
+        </div>
+        <div className="prose flex space-x-5 items-center">
+          <StoryBox></StoryBox>
+          <StoryBox></StoryBox>
+          <ColumnIndicator></ColumnIndicator>
+        </div>
       </div>
       <PageIndicator pageTitle="Forest of Story"></PageIndicator>
-    </> 
+    </div>
   )
 }
 
