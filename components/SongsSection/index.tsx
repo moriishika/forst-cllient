9f62e9ab83bace71ffd21c4e38f34280a222e094
Morii Shikaa <48641207+moriishika@users.noreply.github.com>
2023-06-22 14:51:56 +1000

    Thoughts and hearts structure finished

--- components/SongsSection/index.tsx
+++ components/SongsSection/index.tsx
@@ -1,20 +1,41 @@
 import PageIndicator from "../PageIndicator";
 
 const SongsSection = () => {
-  return(
+  return (
     <>
-    <div>
-      <h1>Songs</h1>
-      <div>
-        Turntable Component
+    <div className="flex  justify-center items-center">
+      <div className="bg-red-500">
+        <h1>Songs</h1>
+        <div>
+          <div>
+            <div>
+              <div className="w-[300px] h-[300px] bg-zinc-500">
+                <img></img>
+              </div>
+              <div></div>
+            </div>
+            <div>
+            </div>
+          </div>
+          <div >
+            <h3>The immortal night of thoughts</h3>
+            <div>
+              <button>Prev</button>
+              <button>Play/Pause</button>
+              <button>Next</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div>
-      <div>
-        Vinyl Tracker
+        <div className="bg-blue-300">
+          <div className="vynl-tracker w-[300px] h-[300px] bg-pink-100"></div>
+          <div>
+            <div>Bar</div>
+            <div>4/13</div>
+          </div>
+        </div>
       </div>
-    </div>
-    <PageIndicator pageTitle="Forest of Songs"></PageIndicator>
+      <PageIndicator pageTitle="Forest of Songs"></PageIndicator>
     </>
   )
 }
--- components/ThoughtSection/index.tsx
+++ components/ThoughtSection/index.tsx
@@ -2,10 +2,17 @@ import PageIndicator from "../PageIndicator";
 
 const ThoughtSection = () => {
   return (
-    <>
-      <h1>This is a Outpouring hearts and thoughts section</h1>
+    <div className="w-full bg-red-300">
+    <div className="flex flex-col w-full bg-blue-200 prose max-w-none">
+      <h1 className="">Thoughts <br></br><u>&</u> Hearts</h1>
+      <div>
+        <h2>The unsolved mystery of why someone likes to betray the ot..</h2>
+        <p>Outpouring of heart by moriishika - 3 min read</p>
+      </div>
+    </div>
+      
       <PageIndicator pageTitle="Forest of outpuring hearts and minds"></PageIndicator>
-    </>
+    </div>
   )
 }
 
